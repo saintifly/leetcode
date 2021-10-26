4c2dd9f064c8e5cf067e74e20153b2ad5914345b
saintifly <sflyzheng@163.com>
2021-10-26 21:43:25 +0800

    第k大的数，使用系统函数，后续最好还是用使用原始排序

---
+++ 数组中的第 k 大的数字.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums_sort = sorted(nums)
+        return nums_sort[-k]
