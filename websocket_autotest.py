f9c3d03470556ce4e0a796d7e7e7af901dd796a3
saintifly <523579740@qq.com>
2022-03-08 18:21:59 +0800

    增加注释，使用更清晰

--- websocket_autotest.py
+++ websocket_autotest.py
@@ -2,7 +2,9 @@
 import time
 from websocket import create_connection
 import websocket
-
+'''
+要同时安装websocket 和websocket-client两个库
+'''
 while 1:
 	ws = websocket.WebSocket()
 	ws.connect("ws://192.168.0.1:8080/xx/xxxx",header = ["Host:xxxx",
@@ -11,5 +13,5 @@ while 1:
 		print(sw.gestatus())
 		print("Receiving...")
 		result = ws.recv()
-		print(str(result))
+		print(str(result)) ##转换字符串很重要，否则会报错
 		ws.close()
